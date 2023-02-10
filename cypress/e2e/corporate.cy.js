5396923b2fc8ce4d7de2d29addf03f4f855b0767
Thimmaraju <thimmaraju418@gmail.com>
2023-02-10 08:04:11 +0530

    worked on us 2345

---
+++ cypress/e2e/corporate.cy.js
@@ -0,0 +1,25 @@
+describe('template spec', () => {
+
+
+    it('Search in google', () => {
+  
+      cy.visit('https://www.google.com/')
+      cy.screenshot()
+  
+      cy.get('input[name="q"]').type("USA{enter}")
+  
+      //asertions 
+  
+    })
+  
+    it('Selecte Telugu language', () => {
+  
+      cy.visit('https://www.google.com/')
+  
+      cy.contains("తెలుగు").click()
+  
+      //Assertions
+  
+    })
+  
+  })
